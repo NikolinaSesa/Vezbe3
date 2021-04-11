0bc970a010f9e1b6a3416ff8f676b0ba90924371
NikolinaSesa <sesa.in11.2019@uns.ac.rs>
2021-04-11 11:33:36 +0200

    initial project commit

---
+++ MreznoProgramiranje/src/demo/Client.java
@@ -0,0 +1,37 @@
+package demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) throws IOException {
+		InetAddress addr = InetAddress.getByName("127.0.0.1");	// localhost
+		int port = 9000;
+		
+		Socket sock = null;		
+		try {
+			sock = new Socket(addr, port);
+
+			String poruka = "Hello world!";
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()), true);
+			writer.println(poruka);
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			String odgovor = reader.readLine();
+
+			System.out.println("Server je odgovorio: " + odgovor);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			sock.close();
+		}
+	}
+
+}
---
+++ MreznoProgramiranje/src/demo/ServerThread.java
@@ -0,0 +1,38 @@
+package demo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+	
+	private Socket sock;
+	
+	public ServerThread(Socket sock) {
+		this.sock = sock;
+	}
+
+	@Override
+	public void run() {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			String zahtev = reader.readLine();
+			System.out.println("Stigao je zahtev: " + zahtev);
+
+			// Simulacija cekanja na obradu zahteva:
+			// Thread.sleep(10000);
+
+			String odgovor = "Hello!";
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()), true);
+			writer.println(odgovor);
+
+			sock.close();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
